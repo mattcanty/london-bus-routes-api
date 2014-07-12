066cab4bc68aee89d75e9521c10420ca8674e898
Matthew Canty <mcanty@sportingindex.com>
2014-07-12 14:50:50 -0400

    need to use port set by heroku

--- src/server.js
+++ src/server.js
@@ -2,19 +2,20 @@ var	http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-  
+
   console.log("Starting server");
-  
+
   function onRequest(request, response) {
-      
+
       console.log("Request received");
-    
+
       var pathname = url.parse(request.url).pathname.split("/", 2)[1];
         console.log("Request for " + pathname + " received");
         route(handle, pathname, response, request);
     }
-    var port = 8080;
-    
+
+    var port = process.env.PORT || 8080;
+
     http.createServer(onRequest).listen(port);
     console.log("Server has started on port " + port);
 }
