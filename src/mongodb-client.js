9644273c2077afea6ceda9ce93dc3a75d97bd439
Matthew Canty <mcanty@sportingindex.com>
2014-07-14 14:51:52 +0000

    Indentations changed?

--- src/mongodb-client.js
+++ src/mongodb-client.js
@@ -2,38 +2,36 @@ var MongoClient = require('mongodb').MongoClient;
 var connectionString = require('./../config').mongo.connectionString;
 
 function get(index, type, data, callback){
-  MongoClient.connect(connectionString, function(err, db) {
-    if(err) { return console.dir(err); }
+	MongoClient.connect(connectionString, function(err, db) {
+		if(err) { return console.dir(err); }
 
-    console.log(data);
+		var collection = db.collection('routes');
 
-    var collection = db.collection('routes');
+		console.log('Got "routes" collection');
 
-    console.log('Got "routes" collection');
-
-    collection.findOne({'_route':data}, function(err, item) {
-      if(err){
-        console.log(err);
-      }else{
-        console.log(item);
-        callback(item);
-      }
-    });
-  });
+		collection.findOne({'_route':data}, function(err, item) {
+			if(err){
+				console.log(err);
+			}else{
+				console.log(item);
+				callback(item);
+			}
+		});
+	});
 }
 
 function bulkIndex(index, type, data, callback){
-  MongoClient.connect(connectionString, function(err, db) {
-    if(err) { return console.dir(err); }
+	MongoClient.connect(connectionString, function(err, db) {
+		if(err) { return console.dir(err); }
 
-    var collection = db.collection('routes');
+		var collection = db.collection('routes');
 
-    collection.insert(data, {w:1}, function(err, result) {
-      if(err){
-        callback(err);
-      }
-    });
-  });
+		collection.insert(data, {w:1}, function(err, result) {
+			if(err){
+				callback(err);
+			}
+		});
+	});
 }
 
 exports.bulkIndex = bulkIndex;
