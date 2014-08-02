b3cf7c79f0354b2e2ccb58cff2e7becdb2eaa91c
Matthew Canty <mcanty@sportingindex.com>
2014-08-02 22:03:24 +0000

    rename request handlers file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var server = require("./src/server");
 var router = require("./src/router");
-var requestHandlers = require('./src/requestHandlers');
+var requestHandlers = require('./src/request-handlers');
 
 console.log("Starting bus routes API");
 
--- src/request-handlers.js
+++ src/request-handlers.js
@@ -4,23 +4,15 @@ var util = require('util');
 var url = require('url');
 
 function route(response, request){
-
-  var method = request.method;
-
-  console.log(request.url);
-
   var query = url.parse(request.url).pathname.split("/")[2];
 
-  if(method === "GET" && query && query !== "") {
+  if(request.method === "GET" && query && query !== "") {
 
     console.log("bus routes api receieved GET /" + query);
 
     busdatafacade.search(query, function (error, result){
 
       if (error) {
-
-        console.log(error);
-
         response.writeHead(error.code, {"Content-Type": "text/json"});
         response.write(JSON.stringify(error));
         response.end();
